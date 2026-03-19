0a57a4129668f786fe8eb6806b2667b91eb15610
Tadiparthi Gnaneswar <gnanitadiparthi@gmail.com>
2026-03-19 23:18:22 +0530

    Update start_edututor.bat

--- project files/start_edututor.bat
+++ project files/start_edututor.bat
@@ -2,7 +2,6 @@
 echo 🚀 Starting EduTutor Backend...
 start cmd /k "cd /d %cd% && uvicorn edututor.main:app --reload"
 timeout /t 3 >nul
-
 echo 🧠 Starting EduTutor Unified Frontend...
 start cmd /k "cd /d %cd% && streamlit run streamlit_app.py"
 
