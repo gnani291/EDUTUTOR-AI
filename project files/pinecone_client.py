2da338deb6a0bd0820d5889003ceb2c976103031
Tadiparthi Gnaneswar <gnanitadiparthi@gmail.com>
2026-03-28 09:13:34 +0530

    Update pinecone_client.py

--- project files/pinecone_client.py
+++ project files/pinecone_client.py
@@ -2,7 +2,6 @@ import os
 from datetime import datetime
 from pinecone import Pinecone, ServerlessSpec
 from dotenv import load_dotenv
-
 load_dotenv()
 
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
