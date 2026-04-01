aaa245afe425b648b5722f2ae54babc563b70f41
Tadiparthi Gnaneswar <gnanitadiparthi@gmail.com>
2026-04-01 22:52:32 +0530

    Update google_classroom_streamlit.py

--- project files/google_classroom_streamlit.py
+++ project files/google_classroom_streamlit.py
@@ -4,6 +4,7 @@ from googleapiclient.discovery import build
 CLIENT_SECRETS_FILE = "client_secret.json"
 
 
+
 SCOPES = [
     "https://www.googleapis.com/auth/classroom.courses.readonly",
     "https://www.googleapis.com/auth/classroom.courseworkmaterials.readonly",
