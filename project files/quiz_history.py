b2efc145bd5871f2d8deb483e47767b7def32937
Tadiparthi Gnaneswar <gnanitadiparthi@gmail.com>
2026-04-01 22:53:21 +0530

    Update quiz_history.py

--- project files/quiz_history.py
+++ project files/quiz_history.py
@@ -2,7 +2,6 @@ import streamlit as st
 from pinecone import Pinecone, ServerlessSpec
 import os
 import json
-
 pinecone = Pinecone(api_key="YOUR_API_KEY")  
 index = pinecone.Index("quiz-submissions")
 def fetch_quiz_history(user_id):
