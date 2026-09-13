159bff2d8f95ba67c7fa6e1d3f97088beccf7c98
Tadiparthi Gnaneswar <gnanitadiparthi@gmail.com>
2026-09-13 14:43:01 +0530

    Refactor pinecone_client.py for clarity and structure

--- project files/pinecone_client.py
+++ project files/pinecone_client.py
@@ -7,6 +7,7 @@ load_dotenv()
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
 
+
 pc = Pinecone(api_key=PINECONE_API_KEY)
 if INDEX_NAME not in [index.name for index in pc.list_indexes()]:
     pc.create_index(
